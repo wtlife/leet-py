ba9a6269907827f0e0b218bf215ea8015ac4e8e0
wutao <wutao@dst56675.cn1.global.ctrip.com>
2020-11-03 16:04:41 +0800

    feat:struct

--- core/hot/hot.py
+++ core/hot/hot.py
@@ -2,7 +2,9 @@ from typing import List
 
 
 class Hot:
-    def is_match(self, s: str, p: str) -> bool:
+
+    # 19.IsMatch 正则表达式匹配
+    def isMatch(self, s: str, p: str) -> bool:
         m = len(s) + 1
         n = len(p) + 1
 
@@ -25,6 +27,7 @@ class Hot:
 
         return dp[len(s)][len(p)]
 
+    # 11. 盛最多水的容器
     def maxArea(self, height: List[int]) -> int:
         ans, left, right = 0, 0, len(height) - 1
         while left <= right:
@@ -35,3 +38,30 @@ class Hot:
             else:
                 right -= 1
         return ans
+
+    # 17. 电话号码的字母组合
+    def letterCombinations(self, digits: str) -> List[str]:
+        ans=[]
+        if not digits or len(digits)<1:
+            return ans
+
+        nts = ['','','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+        def dfs(cur:str,idx:int):
+            if idx == len(digits):
+                ans.append(cur)
+                return
+
+            num = int(digits[idx])
+            value = nts[num]
+            for v in value:
+                dfs(cur+ v,idx+1)
+
+        dfs('',0)
+        return ans
+
+def main():
+    hot = Hot()
+    print(hot.letterCombinations("23"))
+
+if __name__ == "__main__":
+    main()
